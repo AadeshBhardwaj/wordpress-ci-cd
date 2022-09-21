3074f90ff88aee0fb8b89f2945698fef12d1cd35
Aadesh Bhardwaj <50766023+AadeshBhardwaj@users.noreply.github.com>
2022-09-21 17:27:52 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 apt update -y
-apt install nginx php php-fpm php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip -y
+apt install php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip -y
 systemctl restart php7.4-fpm
