ac130b2afdd8d6184b9aad239b661dc7b0cabf87
Aadesh Bhardwaj <50766023+AadeshBhardwaj@users.noreply.github.com>
2022-09-22 12:32:11 +0530

    Delete install_dependencies.sh

--- scripts/install_dependencies.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-sudo apt update
-sudo apt install nginx -y
-sudo apt install php-fpm php-curl php-gd php-intl php-mbstring php-soap php-xml php-xmlrpc php-zip -y
-systemctl restart php7.4-fpm
